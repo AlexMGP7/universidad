6d0cc85375e3cd6ffa74bdac69a6981c3a658262
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-11-16 01:38:01 -0400

    Usando crate rand

--- Rust_practicas/guessing_game/src/main.rs
+++ Rust_practicas/guessing_game/src/main.rs
@@ -1,8 +1,13 @@
 #![allow(non_snake_case)]
 
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 fn main() {
     println!("¡Adivina el número!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", secret_number);
     println!("Por favor, introduce tu adivinanza.");
     let mut guess = String::new();
     io::stdin().read_line(&mut guess)
