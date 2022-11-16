80cd47bd07c2986d2ad232483112cbef1552ea68
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-11-16 01:30:55 -0400

    Agregando crate rand luego de la ejecucion base de la funcion del juego

--- Rust_practicas/guessing_game/src/main.rs
+++ Rust_practicas/guessing_game/src/main.rs
@@ -1,4 +1,11 @@
 #![allow(non_snake_case)]
+
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("¡Adivina el número!");
+    println!("Por favor, introduce tu adivinanza.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess)
+    .expect("Fallo al leer la línea.");
+    println!("Tu adivinanza: {}", guess);
 }
