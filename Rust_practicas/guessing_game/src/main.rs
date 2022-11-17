f81f83640da3c0ef63905847e451eb47e39d6ae3
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-11-17 11:53:13 -0400

    Adding a loop for the program

--- Rust_practicas/guessing_game/src/main.rs
+++ Rust_practicas/guessing_game/src/main.rs
@@ -9,16 +9,21 @@ fn main() {
     println!("¡Adivina el número!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("El numero secreto es {}", secret_number);
-    println!("Por favor, introduce tu adivinanza.");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-    .expect("Fallo al leer la línea.");
-    let guess: u32 = guess.trim().parse()
-    .expect("Please type a number!");
-    println!("Tu adivinanza: {}", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop {
+        println!("Por favor, introduce tu adivinanza.");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+        .expect("Fallo al leer la línea.");
+        let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+        println!("Tu adivinanza: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
 }
