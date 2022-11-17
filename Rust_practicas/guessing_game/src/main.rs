e0ea857f94e17b25c681b5b93e8756e313fad4db
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-11-17 14:32:43 -0400

    Leave it after guess the right number

--- Rust_practicas/guessing_game/src/main.rs
+++ Rust_practicas/guessing_game/src/main.rs
@@ -8,14 +8,16 @@ use rand::Rng;
 fn main() {
     println!("¡Adivina el número!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("El numero secreto es {}", secret_number);
+    //println!("El numero secreto es {}", secret_number);
     loop {
         println!("Por favor, introduce tu adivinanza.");
         let mut guess = String::new();
         io::stdin().read_line(&mut guess)
         .expect("Fallo al leer la línea.");
-        let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         println!("Tu adivinanza: {}", guess);
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
