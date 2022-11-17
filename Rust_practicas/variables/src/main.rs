76927d319675c54ec78be45a08cc4fa41a7ca46d
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-11-17 15:31:11 -0400

    Adding concept of shadowing

--- Rust_practicas/variables/src/main.rs
+++ Rust_practicas/variables/src/main.rs
@@ -1,6 +1,15 @@
+/* //Mutables e inmutables
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6; //Esto daria error si la variable es inmutable
+    println!("The value of x is: {}", x);
+}
+*/
+//Shadowing
 fn main() {
     let x = 5;
-    println!("The value of x is: {}", x);
-    //x = 6; //Esto daria error si la variable es inmutable
+    let x = x + 1;
+    let x = x * 2;
     println!("The value of x is: {}", x);
 }
