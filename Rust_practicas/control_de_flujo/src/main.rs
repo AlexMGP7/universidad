f800f7a6289094650ebd87062604691bbf64e12b
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-11-18 22:51:10 -0400

    Learning for

--- Rust_practicas/control_de_flujo/src/main.rs
+++ Rust_practicas/control_de_flujo/src/main.rs
@@ -32,12 +32,27 @@ fn main() {
     assert_eq!(result, 20);
     println!("{}",result)
 }
-*/
+
 fn main() {
     let mut number = 3;
     while number != 0 {
     println!("{}!", number);
     number = number - 1;
+    }
+    println!("LIFTOFF!!!");
+}
+
+fn main() {
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+    println!("the value is: {}", element);
+    }
+}
+*/
+
+fn main() {
+    for number in (1..4).rev() {
+    println!("{}!", number);
     }
     println!("LIFTOFF!!!");
 }
