04238bd8f313e2de83c6021f00ffab02073dffff
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-11-21 09:30:35 -0400

    a stack with linkedlist in rust

--- Rust_practicas/stack/src/main.rs
+++ Rust_practicas/stack/src/main.rs
@@ -1,3 +1,13 @@
+use std::collections::LinkedList;
+
 fn main() {
-    println!("Hello, world!");
+    let mut list = LinkedList::new();
+    list.push_back( 1); // or push_front
+    list.push_back( 2);
+    list.push_back( 3);
+    println!("Before peek: {:?}", list);
+    println!("peeked {:?}", list.back()); // or front
+    println!("After peek but before pop: {:?}", list);
+    list.pop_back(); // or push_front
+    println!("After pop: {:?}", list);
 }
