c25b794ba58daf2276ffaf060ca965056f21cfd0
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-11-24 13:16:08 -0400

    creando lista doblemente enlazada

--- Rust_practicas/DLK_Stack_Queue/src/main.rs
+++ Rust_practicas/DLK_Stack_Queue/src/main.rs
@@ -1,3 +1,30 @@
+use std::{
+    cell::RefCell,
+    rc::{Rc, Weak},
+};
+
+pub struct Node<T: Copy> {
+    pub value: T,
+    pub next: Option<Rc<RefCell<Node<T>>>>,
+    pub prev: Option<Rc<RefCell<Node<T>>>>,
+}
+
+impl<T: Copy> Node<T> {
+    pub fn new(value: T) -> Self {
+        Node {
+            value,
+            next: None,
+            prev: None,
+        }
+    }
+}
+
+impl<T: Copy> From<Node<T>> for Option<Rc<RefCell<Node<T>>>> {
+    fn from(node: Node<T>) -> Self {
+        Some(Rc::new(RefCell::new(node)))
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
