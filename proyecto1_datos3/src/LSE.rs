163256e8d1e4c88caa38e3941e28546d0b8322c3
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-11-28 16:53:55 -0400

    some corrections

--- proyecto1_datos3/src/LSE.rs
+++ proyecto1_datos3/src/LSE.rs
@@ -1,4 +1,3 @@
-
 #[derive(Clone)]
 enum Lista<T> {
     None,
@@ -7,22 +6,21 @@ enum Lista<T> {
 }
 
 #[derive(Clone)]
-struct Cursor<T> { 
+struct Cursor<T> {
     curr: Lista<T>
 }
 
 impl<T> Lista<T> where T: Copy {
     pub fn nuevo() -> Self {
-        Self::None    
+        Self::None
     }
-    
     pub fn eliminar(&mut self) -> Option<T> {
         match self {
             Self::None => None,
             Self::Cola { item } => {
-              let item = *item;
-              self.a_nada();
-              Some(item)
+                let item = *item;
+                self.a_nada();
+                Some(item)
             },
             Self::Lista { item, next } => {
                 let mut n = Box::new(Self::None);
@@ -33,17 +31,17 @@ impl<T> Lista<T> where T: Copy {
             }
         }
     }
-    
+
     pub fn insertar(&mut self, x: T) {
         match self {
-           Self::None => self.hacia_atras(x),
-           Self::Cola { .. } => self.a_lista(x),
-           Self::Lista { next, .. } => next.insertar(x)
+            Self::None => self.hacia_atras(x),
+            Self::Cola { .. } => self.a_lista(x),
+            Self::Lista { next, .. } => next.insertar(x)
         };
     }
-    
+
     fn a_nada(&mut self) { *self = std::mem::replace(self, Lista::None); }
-    
+
     fn hacia_atras(&mut self, it: T) {
         *self = match self {
             Self::None => Self::Cola { item: it },
@@ -51,14 +49,14 @@ impl<T> Lista<T> where T: Copy {
             _ => panic!("Supplied value was not of correct type or variant.")
         }
     }
-    
+
     fn a_siguiente(&mut self, nxt: Lista<T>) {
         *self = nxt;
     }
-    
+
     fn a_lista(&mut self, x: T) {
         *self = match self {
-            Self::Cola { item } => { 
+            Self::Cola { item } => {
                 Self::Lista { item: *item, next: Box::new(Self::Cola { item: x }) }
             },
             _ => { panic!("no se pudo enviar a la lista"); }
@@ -69,7 +67,7 @@ impl<T> Lista<T> where T: Copy {
 impl<T> IntoIterator for Lista<T> where T: Copy {
     type Item = T;
     type IntoIter = Cursor<T>;
-    
+
     fn into_iter(self) -> Self::IntoIter {
         Cursor {
             curr: self
@@ -79,7 +77,7 @@ impl<T> IntoIterator for Lista<T> where T: Copy {
 
 impl<T> Iterator for Cursor<T> where T: Copy {
     type Item = T;
-    
+
     fn next(&mut self) -> Option<T> {
         let nxt = match self.curr {
             Lista::None => None,
@@ -101,32 +99,32 @@ impl<T> Iterator for Cursor<T> where T: Copy {
 
 pub fn Test() {
 
-    let mut list: Lista<i32> = Lista::nuevo();
-    let mut list2: Lista<i32> = Lista::nuevo();
-    
-    list.insertar(1);
-    list.insertar(2);
-    list.insertar(3);
-    list.insertar(4);
-    
-    list2.insertar(10);
-    list2.insertar(20);
-    list2.insertar(30);
-    
-    println!("{}", list2.eliminar().unwrap());
-    println!("{}", list2.eliminar().unwrap());
-    println!("{}", list2.eliminar().unwrap());
+    let mut lista: Lista<i32> = Lista::nuevo();
+    let mut lista2: Lista<i32> = Lista::nuevo();
+
+    lista.insertar(1);
+    lista.insertar(2);
+    lista.insertar(3);
+    lista.insertar(4);
+
+    lista2.insertar(10);
+    lista2.insertar(20);
+    lista2.insertar(30);
+
+    println!("{}", lista2.eliminar().unwrap());
+    println!("{}", lista2.eliminar().unwrap());
+    println!("{}", lista2.eliminar().unwrap());
     println!("---");
-    
-    for i in list.clone() {
+
+    for i in lista.clone() {
         println!("{}", i);
     }
-    
-    for i in list.clone().into_iter().map(|x| x * 2) {
+
+    for i in lista.clone().into_iter().map(|x| x * 2) {
         println!("{}", i)
     }
-    
-    for (i, x) in list.into_iter().enumerate() {
+
+    for (i, x) in lista.into_iter().enumerate() {
         println!("iter2: {}, {}", i, x);
     }
 }
