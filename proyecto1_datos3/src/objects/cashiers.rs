4bca61d62b40ea789585fa644a523ce40eb5af57
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-12-01 14:23:48 -0400

    improving cashiers

--- proyecto1_datos3/src/objects/cashiers.rs
+++ proyecto1_datos3/src/objects/cashiers.rs
@@ -1,35 +1,58 @@
-use std::collections::VecDeque;
+use std::collections::VecDeque;//Usamos la libreria local VecDeque, que tiene todas las funciones de una cola
+use super::cart::Carrito;//Exportamos los carritos en la cajera
 
-use super::cart::Carrito;
-#[derive(Debug)]
-pub struct Cajera{
+#[derive(Debug)]//Para poder testear y ver todo los atributos que tiene el objeto de manera general
+pub struct Cajera{//Creamos el objeto cajera, la cual sera nuestra "cola" hablando en terminos generales
     pub nom_cajera:String,
     pub dinero_recaudado:f32,
-    pub cola: VecDeque<Carrito>
+    pub linea: VecDeque<Carrito>//Creamos la linea de carritos de la cajera
+    //(Los carritos en espera, es un metodo de la implementacion de la estructura, no un atributo)
 }
 
 impl Cajera{
 
     #[allow(dead_code)]
-    pub fn cajera_nueva(nom_cajera:String,dinero_recaudado:f32) -> Self{
-        Cajera{ nom_cajera, dinero_recaudado, cola:VecDeque::new() }
+    pub fn cajera_nueva(nom_cajera:String,dinero_recaudado:f32) -> Self{//Funcion para crear una cajera nueva
+        Cajera{ nom_cajera, dinero_recaudado, linea:VecDeque::new() }
     }
 
+    #[allow(dead_code)]
     pub fn encolar(&mut self, carrito:Carrito){
-        self.cola.push_back(carrito);
+        self.linea.push_back(carrito);
     }
 
+    #[allow(dead_code)]
     pub fn desencolar(&mut self){
-        self.cola.pop_front();
+        self.linea.pop_front();
     }
 
     #[allow(dead_code)]
     pub fn frente(&mut self) -> Option<&Carrito>{
-        self.cola.front()
+        self.linea.front()
     }
 
     #[allow(dead_code)]
     pub fn fondo(&mut self) -> Option<&Carrito>{
-        self.cola.back()
+        self.linea.back()
+    }
+    #[allow(dead_code)]
+    pub fn carritos_en_espera(&mut self) -> usize{
+        self.linea.len()
+    }
+
+    pub fn sumar_dinero(&mut self, carrito:Carrito){//Sumar el dinero de un carro y agregarlo a total recaudado
+        let dinero_carrito=sumar_productos(carrito);
+        self.dinero_recaudado=self.dinero_recaudado+dinero_carrito;
+        self.desencolar();
+
+        pub fn sumar_productos(mut carrito:Carrito) -> f32{
+            let mut suma=carrito.pop().unwrap().precio;
+            loop {
+                if carrito.cant_productos()==0{
+                    return suma;
+                }
+                suma = suma + carrito.pop().unwrap().precio;
+            }
+        }
     }
 }
