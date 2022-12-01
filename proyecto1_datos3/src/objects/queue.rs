9aeb306e3f275d2d66232ed0373af193e2c33034
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-12-01 00:59:47 -0400

    Creating a queue(cashier list)

---
+++ proyecto1_datos3/src/objects/queue.rs
@@ -0,0 +1,13 @@
+use std::collections::VecDeque;
+use super::cart::Carrito;
+
+#[allow(dead_code)]
+pub struct Cola{
+    pub cola:VecDeque<Carrito>,
+}
+
+/* impl Cola {
+    #[allow(dead_code)]
+    pub fn cola_nueva() -> Self{
+    
+    } */
