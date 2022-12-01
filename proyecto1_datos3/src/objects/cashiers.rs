919a3b9d1a0fa0f7f90b289c81cb2dc74270c57c
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-12-01 01:17:10 -0400

    adding cashiers(queue of carts)

---
+++ proyecto1_datos3/src/objects/cashiers.rs
@@ -0,0 +1,34 @@
+use std::collections::VecDeque;
+
+use super::cart::Carrito;
+#[derive(Debug)]
+pub struct Cajera{
+    pub nom_cajera:String,
+    pub dinero_recaudado:f32,
+    pub cola: VecDeque<Carrito>
+}
+
+impl Cajera{
+
+    #[allow(dead_code)]
+    pub fn cajera_nueva(nom_cajera:String,dinero_recaudado:f32) -> Self{
+        Cajera{ nom_cajera, dinero_recaudado, cola:VecDeque::new() }
+    }
+
+    pub fn encolar(&mut self, carrito:Carrito){
+        self.cola.push_back(carrito);
+    }
+
+    pub fn desencolar(&mut self){
+        self.cola.pop_front();
+    }
+
+    pub fn frente(&mut self) -> Option<&Carrito>{
+        self.cola.front()
+    }
+
+    #[allow(dead_code)]
+    pub fn fondo(&mut self) -> Option<&Carrito>{
+        self.cola.back()
+    }
+}
