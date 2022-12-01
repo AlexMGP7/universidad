e203b38aa200517afdbcbf119c7b392db795fb53
Alexander Gonzalez <agonzalezpastena@gmail.com>
2022-12-01 14:22:06 -0400

    starting to read the text

--- proyecto1_datos3/src/objects/lecture.rs
+++ proyecto1_datos3/src/objects/lecture.rs
@@ -17,30 +17,23 @@ pub fn leer(){
     for x in datos_cajeros1{
         arr_datos_cajero1.push(x);
     }
-    //cola::Queue::cola_nueva(arr_datos_cajero1[0].to_string(), arr_datos_cajero1[1].to_string(), arr_datos_cajero1[2].to_string());
 
     let datos_cajeros2=cajeros[1].split_whitespace();
     let mut arr_datos_cajero2:Vec<&str>=Vec::new();
     for x in datos_cajeros2{
         arr_datos_cajero2.push(x);
     }
-    //cola::Queue::cola_nueva(arr_datos_cajero2[0].to_string(), arr_datos_cajero2[1].to_string(), arr_datos_cajero2[2].to_string());
 
     let datos_cajeros3=cajeros[2].split_whitespace();
     let mut arr_datos_cajero3:Vec<&str>=Vec::new();
     for x in datos_cajeros3{
         arr_datos_cajero3.push(x);
     }
-    //cola::Queue::cola_nueva(arr_datos_cajero3[0].to_string(), arr_datos_cajero3[1].to_string(), arr_datos_cajero3[2].to_string());
-
-
 
     let mut carritos=Vec::new();
     for x in 3..arr_text.len(){
         carritos.push(arr_text[x]);
         println!("[{}]",carritos[x-3]);
     }
-
-    
 
 }
